c3d5f416c92de60f8853e00728c234501353a863
gamjawang <266891848+gamjawang@users.noreply.github.com>
2026-09-15 09:36:02 +0900

    1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
   const name = 'Hello About'
 
   return (
-    <div>
+    <div className="flex flex-1 flex-col items-center justify-center gap-6 px-8 py-24 text-center">
       <h1>이 페이지는 app/about/page.tsx 입니다.</h1>
       <p>{name}</p>
       <Link href="/">Home으로 이동하기</Link>
